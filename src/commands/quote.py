7e1b69386d077138b9b839b5ea408767c5a8054b
Dragonsight91 <r.vanemelen@icloud.com>
2020-03-17 12:40:34 +0100

    forgot to reset limiter after execution...

--- src/commands/quote.py
+++ src/commands/quote.py
@@ -5,14 +5,20 @@ async def quote_handler(bot:discord.Client, message:discord.Message, command:lis
     try:
         ratelimit = 800
         lastcall = int(time() - bot.lastCall["quote"])
+        print(ratelimit)
+        print(lastcall)
+        
         if lastcall <= ratelimit:
             await message.channel.send(f'**{bot.izzymojis["izzyangry"]} RATE LIMIT**\nSorry, but this command has a rate limit to prevent spam. please try again in `{ratelimit - lastcall} Seconds`')
-        if command[1] == "inspirobot":
+            return
+
+        elif command[1] == "inspirobot":
             response = await bot.inspirobot.get_quote()
             e = discord.Embed(colour=0xff00e7, title="Quote from Inspirobot")
             e.set_image(url=response)
             
             await message.channel.send(embed=e)
+            bot.lastCall = time()
     # NO NO NO NO, WHY U DO DIS???
     except Exception as e:
         await message.channel.send(f"**{bot.izzymojis['izzyangry']} IZZY**\nHey {devRole.mention} There was an error.\n```\n{e}\n```")
