e9fb2c051e333c626f44d4484a002831bd413559
Dragonsight91 <r.vanemelen@icloud.com>
2020-03-17 12:25:10 +0100

    add title to response

--- src/commands/quote.py
+++ src/commands/quote.py
@@ -4,8 +4,9 @@ async def quote_handler(bot:discord.Client, message:discord.Message, command:lis
     try:
         if command[1] == "inspirobot":
             response = await bot.inspirobot.get_quote()
-            e = discord.Embed(colour=0xff00e7)
+            e = discord.Embed(colour=0xff00e7, title="Quote from Inspirobot")
             e.set_image(url=response)
+            
             await message.channel.send(embed=e)
     # NO NO NO NO, WHY U DO DIS???
     except Exception as e:
