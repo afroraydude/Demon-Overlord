245520012ada08737f2a240b51fbaeff4e3d3813
Dragonsight91 <r.vanemelen@icloud.com>
2020-03-20 01:05:22 +0100

    make quote standalone

--- src/commands/quote.py
+++ src/commands/quote.py
@@ -15,7 +15,7 @@ async def quote_handler(bot:discord.Client, message:discord.Message, command:lis
             await message.channel.send(f'**{bot.izzymojis["izzyangry"]} RATE LIMIT**\nSorry, but this command has a rate limit to prevent spam. please try again in `{minutes} Minutes {seconds} Seconds`')
             return
 
-        elif command[1] == "inspirobot":
+        else:
             response = await bot.inspirobot.get_quote()
             e = discord.Embed(colour=0xff00e7, title="Quote from Inspirobot")
             e.set_image(url=response)
