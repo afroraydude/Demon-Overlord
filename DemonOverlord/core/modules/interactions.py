8c6985a6d35ee27ca699f16f7184608142a1b6c8
Dragonsight91 <r.vanemelen@icloud.com>
2020-03-21 13:54:23 +0100

    add more modules and change structure

---
+++ DemonOverlord/core/modules/interactions.py
@@ -0,0 +1,46 @@
+import discord
+
+
+from DemonOverlord.core.util.responses import ImageResponse
+from DemonOverlord.core.util.api import TenorAPI
+
+
+
+# base interaction
+class Interaction(ImageResponse):
+    def __init__(self, type: dict, user: discord.Member, color: int = 0xffffff):
+        super().__init__(title=f'', color=color, )
+        self.type = type
+        self.user = user
+
+    def add_message(self, msg:str) -> None:
+        self.add_field(name="Message:", value=msg)
+
+    @staticmethod
+    async def handler(bot:discord.Client ) -> ImageResponse:
+        pass
+
+# social Interaction
+class SocialInteraction(Interaction):
+    def __init__(self, type: dict, user: discord.Member):
+        super().__init__(type, user, color=0xa251af)
+
+
+# music interaction, a special case that has both Alone and Social aspects
+class MusicInteraction(Interaction):
+    def __init__(self, type: dict, user: discord.Member):
+        super().__init__(type, user, 0x1db954)
+
+        spotify = list(filter(lambda x: isinstance(
+            x, discord.Spotify), user.activities))
+        self.spotify = spotify[0] if len(spotify) > 0 else None
+
+        if self.spotify != None:
+            self.add_field(name=self.spotify.title,
+                           value=self.spotify.artist, inline=False)
+
+
+# alone interaction, stuff you do yourself
+class AloneInteractions(Interaction):
+    def __init__(self, type: dict, user: discord.Member):
+        super().__init__(type, user, color=0xe2268f)
---
+++ DemonOverlord/core/modules/relationship.py
@@ -0,0 +1,67 @@
+import discord
+
+# user JSON object
+# "user_{uid}": {
+#     "uid":"{uid}"
+#     "relationships": {
+#         "good": {
+#             "user_{uid}": {
+#                 "status": "friend"
+#             },
+#             "user_{uid}": {
+#                 "status": "enemy"
+#             }
+#         },
+#         "bad": {}
+#     }
+# }
+
+
+class User:
+    __slots__ = (
+        "uid", "relationships", "build_relationship", "__dict__"
+    )
+
+    def __init__(self, user: discord.Member = None, user_dict: dict = None):
+        self.uid = user.id
+        self.relationships = []
+
+        if user_dict:
+            for key in user_dict["relationship"]:
+                rel = User.build_relationship(user_dict)
+                self.relationships.append()
+
+    @staticmethod
+    def build_relationship(self, uid: int) -> Relationship:
+        pass
+
+    def __dict__(self) -> dict:
+        temp = dict()
+        temp["uid"] = self.uid
+        temp["relationships"] = dict(good=dict(), bad=dict())
+
+        for elem in self.relationships:
+            if isinstance(elem, GoodRelationship):
+                temp["relationships"]["good"][f'uid_{elem.uid}'] = elem.name
+
+        return temp
+
+
+class Relationship:
+    __slots__ = (
+        "user", "status"
+    )
+    def __init__(self, status: str, user: User):
+        self.user = user
+        self.status = status
+
+
+class GoodRelationship(Relationship):
+    def __init__(self, status: str, user: User):
+        super().__init__(status, user)
+
+
+class BadRelationship(Relationship):
+    def __init__(self, status: str, user: User):
+        super().__init__(status, user)
+
